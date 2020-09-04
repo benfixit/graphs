4ade2c80d7d3346d484282a1902e1320a1ea5374
Chukwuemeka Inya <inyachukwuemeka1@gmail.com>
2020-09-04 11:08:35 +0100

    Move back depth first search to class

--- index.js
+++ index.js
@@ -1,41 +1,63 @@
-const breadthFirstSearch = require('./exercises/breath_first_search');
-const depthFirstSearch = require('./exercises/depth_first_search');
-
 class Graph {
     constructor(v){
-        this.vertices = v;
         this.adjList = [];
-        this.edges = 0;
         this.visited = [];
+        this.vertices = v;
 
-        for (let i = 0; i < this.vertices; i++){
-            this.adjList[i] = [];
+        for(let i = 0; i < this.vertices; i++){
+            this.adjList[i] = []
         }
-
-        this.breadthFirstSearch = breadthFirstSearch
-        this.depthFirstSearch = depthFirstSearch
     }
 
     addEdge = (v, w) => {
         this.adjList[v].push(w);
         this.adjList[w].push(v);
-        this.edges++;
     }
 
-    showGraph = () => {
-        for(let i = 0; i < this.adjList.length; i++){
-            let str = `${i} -> `;
-            for (let j = 0; j < this.adjList[i].length; j++){
-                str += ` ${this.adjList[i][j]}`;
+    depthFirstSearchRecursive = (vertex) => {
+        this.visited[vertex] = true;
+        console.log('Visited === ', vertex);
+        for(let i = 0; i < this.adjList[vertex].length; i++){
+            const tempVert = this.adjList[vertex][i]
+            if(!this.visited[tempVert]) {
+                this.depthFirstSearchRecursive(tempVert);
             }
-            console.log(str);
         }
+    }
+
+    depthFirstSearchIterative = (vertex) => {
+        let stack = [];
+        stack.push(vertex);
+        while(stack.length > 0){
+            const tempVertex = stack.pop();
+
+            if(!this.visited[tempVertex]){
+                this.visited[tempVertex] = true;
+                console.log('Vertex === ', tempVertex);
+            }
+
+            for(let i = 0; i < this.adjList[tempVertex].length; i++){
+                const newVertex = this.adjList[tempVertex][i];
+                if(!this.visited[newVertex]){
+                    stack.push(newVertex);
+                }
+            }
+        }
+    }
+
+    breadthFirstSearch = () => {
+
     }
 }
 
-const gr = new Graph(5);
-gr.addEdge(0, 1)
-gr.addEdge(0, 2)
-gr.addEdge(1, 3)
-gr.addEdge(2, 4)
-console.log(gr.depthFirstSearch(0));
+const g = new Graph(5);
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 3)
+g.addEdge(2, 4)
+
+g.depthFirstSearchIterative(0);
+
+// console.log(gr.adjList)
+
+
